3b64aa9945cf6e1346fdb2c58084bf9748335cb9
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-21 18:56:09 -0300

    refatorado metodo incluir tornando o indepente - Polimorfismo

--- src/calculations/CalculadoraDeTempo.java
+++ src/calculations/CalculadoraDeTempo.java
@@ -1,7 +1,6 @@
 package calculations;
 
-import br.com.alura.screenmatch.models.Filme;
-import br.com.alura.screenmatch.models.Serie;
+import br.com.alura.screenmatch.models.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal;
@@ -10,10 +9,14 @@ public class CalculadoraDeTempo {
         return tempoTotal;
     }
 
-    public void incluir(Filme filme) {
+ /*   public void incluir(Filme filme) {
         tempoTotal += filme.getDuracaoEmMinutos();
     }
     public void incluir(Serie serie) {
         tempoTotal += serie.getDuracaoEmMinutos();
+    }*/
+    public void incluir(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 }
