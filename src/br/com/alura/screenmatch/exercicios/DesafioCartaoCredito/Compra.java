09a5627dfbab207b47fc148e161b4d69fb9e0426
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-11 15:56:45 -0300

    wip: criado metodo de lancamento de compra na lista

--- src/br/com/alura/screenmatch/exercicios/DesafioCartaoCredito/Compra.java
+++ src/br/com/alura/screenmatch/exercicios/DesafioCartaoCredito/Compra.java
@@ -1,6 +1,25 @@
 package br.com.alura.screenmatch.exercicios.DesafioCartaoCredito;
 
-public class
-Compra {
+public class Compra {
+    private String descricao;
+    private double valor;
 
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descrição = " +
+                " valor = " + valor;
+    }
 }
