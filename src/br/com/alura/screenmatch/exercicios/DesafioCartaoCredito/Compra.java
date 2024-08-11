15a879401a9d75c46ba223fcb1f32c7dc8b3bb73
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-11 16:32:25 -0300

    implementado menu de entradas e a interface Comparable

--- src/br/com/alura/screenmatch/exercicios/DesafioCartaoCredito/Compra.java
+++ src/br/com/alura/screenmatch/exercicios/DesafioCartaoCredito/Compra.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.exercicios.DesafioCartaoCredito;
 
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private String descricao;
     private double valor;
 
@@ -19,7 +19,12 @@ public class Compra {
 
     @Override
     public String toString() {
-        return "Compra: descrição = " +
-                " valor = " + valor;
+        return "Compra: descricao = " + descricao +
+                " valor =" + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
     }
 }
